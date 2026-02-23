738b79ae6f7005e42eb0b7ae1944c14a6c54ec94
Prashannt Bhandari <prashantbhandaricr07@gmail.com>
2026-02-23 20:47:49 +0530

    initail commit , start with creating user service and db config and connection

---
+++ userService/src/main/java/com/fitness_micro/userService/model/User.java
@@ -0,0 +1,40 @@
+package com.fitness_micro.userService.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private  String firstName;
+    private String lastName;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role = UserRole.USER;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private  LocalDateTime updatedAt;
+
+}
---
+++ userService/src/main/java/com/fitness_micro/userService/model/UserRole.java
@@ -0,0 +1,5 @@
+package com.fitness_micro.userService.model;
+
+public enum UserRole {
+    USER, ADMIN
+}
